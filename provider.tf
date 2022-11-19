508b216a21ad7132d8ffc82905a4c4d04100684e
Digsingh86 <107998612+Digsingh86@users.noreply.github.com>
2022-11-19 21:03:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,7 @@
 #provider.tf
 
-
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
+provider "aws"{
+region = "us-east-1"
+access_key = "AKIAUBH3OCP6YFFTUNPT"
+secret_key = "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"
 }
