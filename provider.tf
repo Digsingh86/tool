6703e7038ad488440d7c5885a539fb04ed7f1e12
Digsingh86 <107998612+Digsingh86@users.noreply.github.com>
2022-11-19 20:26:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,13 @@
-provider "aws" {
-  region = "ap-southeast-1"
-  shared_credentials_file = "cred.yml"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
